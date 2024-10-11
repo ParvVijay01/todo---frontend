835886a6ea8b3cc44cf5d9e2d1c2ffafd5a13488
ParvVijay01 <parvvj2000@gmail.com>
2024-10-12 02:04:08 +0530

    1. Sign up and login page. 2. Connected backend

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+const url = 'http://172.25.80.1:3000/';
+const registration = "${url}registration";
+const login = '${url}login';
+const addtodo = '${url}storeTodo';
+const getToDoList = '${url}getUserTodoList';
+const deleteTodo = '${url}deleteTodo';
